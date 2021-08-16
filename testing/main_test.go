5e3662be703529f9ec2893cdffb23585dd6c1b36
Felipe Ospina <pipe.98.oh@gmail.com>
2021-08-15 20:26:04 -0500

    CPU Profiling

--- testing/main_test.go
+++ testing/main_test.go
@@ -41,7 +41,25 @@ func TestMax(t *testing.T) {
 	for _, item := range tables {
 		max := GetMax(item.a, item.b)
 		if max != item.n {
-			t.Errorf("Sum was incorrect, got %d expected %d", max, item.n)
+			t.Errorf("Max was incorrect, got %d expected %d", max, item.n)
+		}
+	}
+}
+
+func TestFib(t *testing.T) {
+	tables := []struct {
+		a int
+		n int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+
+	for _, item := range tables {
+		fib := Fibonacci(item.a)
+		if fib != item.n {
+			t.Errorf("Fibonacci was incorrect, got %d expected %d", fib, item.n)
 		}
 	}
 }
